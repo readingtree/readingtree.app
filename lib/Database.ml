79558af482e8d2a29fd35460c1c777c510b4c9b7
Rawley Fowler <fowlerr@fcc.ca>
2023-09-26 21:19:48 -0600

    start db

--- lib/Database.ml
+++ lib/Database.ml
@@ -1,21 +1,27 @@
 open Lwt.Syntax
 open Piaf
-open Yojson
 
 module Http = Client.Oneshot
 
 type encoding_type = Json | Text
+type couch_response =
+  | Json_response of Yojson.Safe.t
+  | Text_response of string
 
 let ( >|= ) = Result.bind
+let ( >>|= ) = fun a b ->
+  match a with
+  | Ok t -> b t
+  | Error e -> Lwt.return (Error ( Error.to_string e ))
 
 let db_uri =
-  match Sys.get_env_opt "COUCHDB_URI" with
+  match Sys.getenv_opt "COUCHDB_URI" with
   | Some uri -> uri
   | None -> failwith "COUCHDB_URI must be set."
 
 let auth_credential =
-  match ( Sys.get_env_opt "COUCHDB_USERNAME"
-        , Sys.get_env_opt "COUCHDB_PASSWORD" )
+  match ( Sys.getenv_opt "COUCHDB_USERNAME"
+        , Sys.getenv_opt "COUCHDB_PASSWORD" )
   with
   | ( Some username
     , Some password ) -> Base64.encode_exn (username ^ ":" ^ password)
@@ -23,19 +29,45 @@ let auth_credential =
 
 let standard_headers =
   [ ("Content-Type", "application/json")
+  ; ("Accept", "application/json")
   ; ("Authorization", "Basic " ^ auth_credential) ]
 
-let encode_response ~encoding res =
+let encode_response ~(encoding : encoding_type) (res : Piaf.Response.t) =
   if Status.is_successful res.status then
-    match encoding with
-    | Json -> Ok( YoJson.Safe.from_string @@ Body.to_string res.body)
-    | Text -> Ok( Body.to_string res.body )
+    let+ body = Body.to_string res.body in
+    Result.map_error Error.to_string (
+        body >|=
+          fun b ->
+          Ok( match encoding with
+              | Json -> Json_response( Yojson.Safe.from_string b )
+              | Text -> Text_response( b )
+            )
+      )
   else
-    Error (Status.to_string res.status)
+    Lwt.return ( Error(Status.to_string res.status) )
+
+let create_db ~name =
+  let* response =
+    Http.put
+      ~headers:standard_headers
+      (Uri.of_string (db_uri ^ "/" ^ name))
+  in
+  response >>|= encode_response ~encoding:Json
 
 let find_list ~id =
-  Http.post
-    ~headers:standard_headers
-    ~body:(Printf.sprintf {json|{_id: %s}|json} id)
-    (db_uri ^ "/lists/_find")
-  >|= encode_response ~encoding:Json
+  let* response =
+    Http.post
+      ~headers:standard_headers
+      ~body:(Body.of_string @@ Printf.sprintf {json|{_id: %s}|json} id)
+      (Uri.of_string (db_uri ^ "/lists/_find"))
+  in
+  response >>|= encode_response ~encoding:Json
+
+let save_list ~list =
+  let* response =
+    Http.put
+      ~headers:standard_headers
+      ~body:(Body.of_string @@ Yojson.Safe.to_string list)
+      (Uri.of_string (db_uri ^ "/lists/"))
+  in
+  response >>|= encode_response ~encoding:Json
