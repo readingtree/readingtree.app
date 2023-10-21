c619c6c3781b11eaf6786326b25dff12839e8350
Rawley Fowler <rawleyfowler@proton.me>
2023-10-20 20:01:32 -0600

    add modal

--- lib/handler/Handler.ml
+++ lib/handler/Handler.ml
@@ -107,7 +107,7 @@ let tree_view_handler request =
       | Ok (`String description) ->
         Dream.html @@
         View.Tree.render
-          ~scripts:[ "https://unpkg.com/vis-network@9.1.8/dist/vis-network.min.js"]
+          ~scripts:[ "https://cdnjs.cloudflare.com/ajax/libs/vis-network/9.1.8/standalone/umd/vis-network.min.js" ]
           ~description
           request
       | Ok _ -> Dream.html ~status:`Not_Found @@ View.NotFound.render request
--- lib/view/Tree.ml
+++ lib/view/Tree.ml
@@ -14,6 +14,17 @@ let render
       [ h1 [ txt description ]
       ; (if Util.Auth.is_admin request then Partial.Tree_admin_form.render ~id request else (div []))
       ; div ~a:[ a_class [ "vw-100"; "vh-100" ]; a_id "tree" ] []
+      ; Partial.Modal.render
+          ~id:"book-modal"
+          ~title:"Book"
+          ~content:[]
+          ~footer:
+            [ button
+                ~a:[ a_class [ "btn"; "btn-success"]
+                   ; a_button_type `Button
+                   ]
+                [ txt "Mark as Read" ]
+            ]
       ]
       request
   in
